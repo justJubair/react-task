35ff1fed1191d383ee5d190b4fe349f4e6b165d3
Jubair <jubair.ahmed7946@gmail.com>
2023-12-15 01:08:42 +0600

    Add: example commit

--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -25,7 +25,7 @@ const Problem2 = () => {
   return (
     <div className="container">
       <div className="row justify-content-center mt-5">
-        <h4 className="text-center text-uppercase mb-5">Problem-2</h4>
+        <h4 className="text-center text-uppercase mb-5">Probm-2</h4>
 
         <div className="d-flex justify-content-center gap-3">
           <button
